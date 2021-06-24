b7e198ce4ef444f3f15f60c73f577ea231243a3e
edat86 <dalvarado@estratega.ec>
2021-06-24 08:32:08 -0500

    Agregando estilos y instalando bootstrap por npm

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,23 +1,29 @@
 import React from 'react';
+
+import "./styles/Badge.css";
 import conflogo from '../images/badge-header.svg';
+
+
 class Badge extends React.Component{
     render () {
         return (
-           <div>
-                <div>
+           <div className="Badge">
+                <div className="Badge__header">
                     <img src={conflogo} alt="Logo de la conferencia"></img>
                 </div>
                
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" 
+                        src="http://1.gravatar.com/avatar/3193d0e6c6859d752a3cf338eba913da" 
+                        alt="Avatar"></img>
                     <h1>Daniel <br/> Alvarado T.</h1>
                 </div>
 
-                <div>
+                <div className="Badge__section-info">
                     <p>Fronted Engineer</p>
                     <p>@dalvarado</p>
                 </div>
-                <div>
+                <div className="Badge__footer">
                     #platziconf
                 </div>
            </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import 'bootstrap/dist/css/bootstrap.css';
 //Importar Componente
 import Badge from './components/Badge';
 
